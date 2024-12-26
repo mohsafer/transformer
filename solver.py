9ebf7ec44fe83b00f3b8ddf091e1a00cdfcaca9e
mohsafer <mohsafer@gmail.com>
2024-12-26 13:54:18 -0500

    ddd

--- solver.py
+++ solver.py
@@ -15,7 +15,7 @@ warnings.filterwarnings('ignore')
 #comment from vscode Ssh
 #second commit fromssh hahahahahah
 writer = SummaryWriter()
-
+#dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
 def my_kl_loss(p, q):
     res = p * (torch.log(p + 0.0001) - torch.log(q + 0.0001))
     return torch.mean(torch.sum(res, dim=-1), dim=1)
