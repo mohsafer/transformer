e6155d629fc1b15bfec099d8259304884ea4ddef
mohsafer <mohsafer@gmail.com>
2024-12-27 14:07:05 -0500

    ds

--- solver.py
+++ solver.py
@@ -408,15 +408,15 @@ class Solver(object):
         # plt.show()
         # writer.close()
         # Function to extract a random segment of length 150
-        def extract_random_segment(data, segment_length=10000):
+        def extract_random_segment(data, segment_length=200):
             if len(data) <= segment_length:
                 return data  # Return the entire data if it's shorter than the segment length
             start_idx = np.random.randint(0, len(data) - segment_length)
             return data[start_idx:start_idx + segment_length]
 
         # Extract random segments of length 150
-        segment_length = 10000
-        test_energy_segment = extract_random_segment(test_energy, segment_length)
+        segment_length = 200
+        test_energy_segment = extract_random_segment(pred, segment_length)
         gt_segment = extract_random_segment(gt, segment_length)
 
         # Plot the random segment
